8650012babe6d080a6971bf717b63d3fa723d9f3
Sander Van Dooren <sander.vandooren@one-agency.be>
2016-11-02 23:35:36 +0100

    Initial commit of indexer.

---
+++ mockfs-index.c
@@ -0,0 +1,116 @@
+
+#include "mockfs-index.h"
+#include <getopt.h>
+
+/* Flag set by ‘--verbose’. */
+static int verbose_flag;
+
+/**
+ * Callback for sqlite.
+ */
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+    return 0;
+}
+
+
+int main(int argc, char *argv[]) {
+    char *source_path = ".";
+    char *output_file = "fileindex";
+
+    int c;
+
+    // Parse cli options.
+    while (1) {
+        static struct option long_options[] = {
+                /* These options set a flag. */
+                {"verbose", no_argument,       &verbose_flag, 1},
+                {"brief",   no_argument,       &verbose_flag, 0},
+                /* These options don’t set a flag.
+                   We distinguish them by their indices. */
+                {"source",  required_argument, 0, 's'},
+                {"output",  required_argument, 0, 'o'},
+                {0, 0, 0, 0}
+        };
+        int option_index = 0;
+        c = getopt_long(argc, argv, "o:", long_options, &option_index);
+        if (c == -1)
+            break;
+        switch (c) {
+            case 0:
+                /* If this option set a flag, do nothing else now. */
+                if (long_options[option_index].flag != 0)
+                    break;
+            case 's':
+                source_path = optarg;
+            case 'o':
+                output_file = optarg;
+                break;
+            case '?':
+                printf("Usage: mockfs-index -o index");
+        }
+    }
+    unlink(output_file);
+
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+    char sql[500];
+
+    /* Open database */
+    rc = sqlite3_open(output_file, &db);
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        exit(EXIT_FAILURE);
+    }
+
+    /* Create SQL statement */
+    strcpy(sql, "CREATE TABLE FILESYSTEM("  \
+         "INO      UNSIGNED BIG INT     PRIMARY KEY," \
+         "P_INO    UNSIGNED BIG INT     NOT NULL," \
+         "NAME     TEXT                 NOT NULL," \
+         "MODE     INT                  NOT NULL);");
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+        exit(EXIT_FAILURE);
+    }
+
+    // Define the callback as an inline function, so we can use the variables inside main's scope.
+    int list(const char *pathname, const struct stat *status, int type, struct FTW *ftwbuf) {
+        char parent_path[PATH_SIZE];
+        static int first = 1;
+        // Set the inode id of the root dir to 0, as to have a starting point.
+        __ino_t parent_ino = 0;
+        strcpy(parent_path, pathname);
+        parent_path[ftwbuf->base] = '\0';
+        struct stat st;
+        if (verbose_flag) {
+            for(int i=1; i<ftwbuf->level;i++)
+                printf("-");
+            printf("%s\n", &pathname[ftwbuf->base]);
+        }
+
+        if (!first) {
+            stat(parent_path, &st);
+            parent_ino = st.st_ino;
+        }
+        sprintf(sql, INSERT_QUERY, status->st_ino, parent_ino, &pathname[ftwbuf->base], status->st_mode);
+        rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "SQL error: %s\n", zErrMsg);
+            sqlite3_free(zErrMsg);
+            exit(EXIT_FAILURE);
+        }
+        first = 0;
+        return 0;
+    }
+
+    // Start walking the filesystem.
+    int flags = 0;
+    nftw(source_path, list, 1, flags);
+
+    return 0;
+}
---
+++ mockfs-index.h
@@ -0,0 +1,26 @@
+//
+// Created by sander on 02.11.16.
+//
+
+#ifndef MOCKFS_MOCKFS_INDEX_H
+#define MOCKFS_MOCKFS_INDEX_H
+
+#define _XOPEN_SOURCE 500
+
+#include <ftw.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sqlite3.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+#define PATH_SIZE 255
+
+#define INSERT_QUERY "INSERT INTO FILESYSTEM(INO,P_INO,NAME,MODE) "  \
+         "VALUES (%lu, %lu, \"%s\", %d);"
+
+#endif //MOCKFS_MOCKFS_INDEX_H
+
+int handle(char *dir, int *parent_inode);
+int get_inode_from_path(char *path);
